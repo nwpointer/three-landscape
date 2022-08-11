880c068b048bcae3f548a0ba5b31683921c974f8
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro.local>
2022-08-10 18:42:56 -0700

    clened out

---
+++ examples/highlands@0.4/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Scene } from './components/Scene'
+
+function App() {
+  return <Scene />
+}
+
+export default App
---
+++ examples/highlands@0.4/src/components/three-landscape/SplatMaterial.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { RepeatWrapping } from "three";
+import SplatStandardMaterialImpl from './three/SplatMaterial'
+
+export const SplatStandardMaterial = React.forwardRef((props, ref) => {
+  const [material] = React.useState(() => new SplatStandardMaterialImpl(props));
+
+  const { diffuseMaps, normalMaps } = props;
+
+  [...diffuseMaps, ...normalMaps].forEach(t => {
+    t.wrapS = RepeatWrapping;
+    t.wrapT = RepeatWrapping;
+  })
+
+  return <primitive dispose={undefined} object={material} ref={ref} attach="material" {...props} />;
+});
+
