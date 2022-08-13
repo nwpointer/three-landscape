cfde840491d690dd7cb0712a46bfc3dabf8d4254
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro.local>
2022-08-13 16:46:26 -0700

    simple mixing!

--- package/src/components/TerrainMaterial.tsx
+++ package/src/components/TerrainMaterial.tsx
@@ -1,21 +1,47 @@
 import React from "react";
 import CustomShaderMaterial from "three-custom-shader-material";
-import { MeshStandardMaterial } from "three";
+import { MeshStandardMaterial, Texture } from "three";
 import glsl from "glslify";
 
-export default function TerrainMaterial({}) {
+export type splatMaterial = {
+  diffuse: Texture;
+  normal?: Texture;
+};
+
+export default function TerrainMaterial(props: {
+  wireframe?: boolean;
+  materials: splatMaterial[];
+  map?: Texture;
+  splats: Texture[];
+}) {
+  const diffuse = props.materials.map((v) => v.diffuse);
+  const normal = props.materials.map((v) => v.normal);
   return (
-    // @ts-expect-error
     <CustomShaderMaterial
       baseMaterial={MeshStandardMaterial}
+      wireframe={props.wireframe}
+      map={props.map}
+      uniforms={{
+        uSplats: { value: props.splats },
+        uDiffuse: { value: diffuse },
+        uNormal: { value: normal },
+      }}
       fragmentShader={glsl`
         precision mediump float;
         #pragma vscode_glsllint_stage : frag
         #ifndef csm_DiffuseColor
           vec4 csm_DiffuseColor = vec4(0,0,0,0);
         #endif
+
+        uniform sampler2D uSplats[${props.splats.length}];
+        uniform sampler2D uDiffuse[${diffuse.length}];
+        uniform sampler2D uNormal[${normal.length}];
+
         void main(){
-          csm_DiffuseColor = vec4(1,0,0,1);
+          vec4 d1 = texture2D(uDiffuse[0], vUv);
+          vec4 d2 = texture2D(uDiffuse[1], vUv);
+          float alpha =  texture2D(uSplats[0], vUv).r;
+          csm_DiffuseColor = mix(d1, d2, alpha);
         }
       `}
     />
--- package/src/index.tsx
+++ package/src/index.tsx
@@ -2,15 +2,4 @@ import React from "react";
 import "@react-three/fiber";
 import TerrainMaterial from "./components/TerrainMaterial";
 
-export function Cube(params: {
-  // color: string;
-  position: [number, number, number];
-}) {
-  return (
-    <mesh position={params.position || [1, 0, 0]}>
-      <boxGeometry />
-      {/* <meshStandardMaterial color={params.color || "red"} /> */}
-      <TerrainMaterial />
-    </mesh>
-  );
-}
+export { TerrainMaterial };
