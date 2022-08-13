ed6773816c1620ccca1fbb4d0f2a873ffc4d6939
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro.local>
2022-08-13 16:46:26 -0700

    simple mixing!

--- examples/hello-world-ts/src/App.tsx
+++ examples/hello-world-ts/src/App.tsx
@@ -1,13 +1,46 @@
 import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Cube } from "three-landscape";
+import { TerrainMaterial } from "three-landscape";
+import { OrbitControls, useTexture } from "@react-three/drei";
+
+function Terrain() {
+  const textures = useTexture([
+    "/splat.jpg",
+    "/Grass_021/ground_Grass1_col.jpg",
+    "/Grass_021/ground_Grass1_norm.jpg",
+    "/Mud_030/Ground_WetBumpyMud_col.jpg",
+    "/Mud_030/Ground_WetBumpyMud_norm.jpg",
+  ]);
+
+  return (
+    <mesh position={[0, 0, 0]}>
+      <planeBufferGeometry args={[4, 4, 10, 10]} />
+      <TerrainMaterial
+        map={textures[0]}
+        splats={[textures[0]]}
+        materials={[
+          // grass
+          {
+            diffuse: textures[1],
+            normal: textures[2],
+          },
+          // mud
+          {
+            diffuse: textures[3],
+            normal: textures[4],
+          },
+        ]}
+      />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <Canvas>
+      <OrbitControls />
       <ambientLight intensity={0.5} />
-      <Cube position={[1, 0, 0]} />
-      <Cube position={[0, 1, 0]} />
+      <Terrain />
     </Canvas>
   );
 }
