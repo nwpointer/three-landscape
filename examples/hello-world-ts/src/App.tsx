5a90a61113fd81efe4be632d2fe00af85181c171
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro.local>
2022-08-14 10:30:52 -0700

    mode based mixxing (bw, rgb, rgba)

--- examples/hello-world-ts/src/App.tsx
+++ examples/hello-world-ts/src/App.tsx
@@ -2,42 +2,86 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { TerrainMaterial } from "three-landscape";
 import { OrbitControls, useTexture } from "@react-three/drei";
+import { RepeatWrapping, Texture } from "three";
+
+const repeatTexture = (t: Texture) => (t.wrapS = t.wrapT = RepeatWrapping);
+
+// function Terrain() {
+//   const textures = useTexture([
+//     "/splat-bw.jpg",
+//     "/Grass_021/ground_Grass1_col.jpg",
+//     "/Grass_021/ground_Grass1_norm.jpg",
+//     "/Mud_030/Ground_WetBumpyMud_col.jpg",
+//     "/Mud_030/Ground_WetBumpyMud_norm.jpg",
+//   ]);
+
+//   textures.map(repeatTexture);
+
+//   const grass = {
+//     diffuse: textures[1],
+//     normal: textures[2],
+//     repeat: 10,
+//   };
+
+//   const mud = {
+//     diffuse: textures[3],
+//     normal: textures[4],
+//     repeat: 2,
+//   };
+
+//   // example bw
+//   return (
+//     <mesh position={[0, 0, 0]}>
+//       <planeBufferGeometry args={[4, 4, 10, 10]} />
+//       <TerrainMaterial map={textures[0]} splats={[textures[0]]} materials={[grass, mud]} />
+//     </mesh>
+//   );
+// }
 
 function Terrain() {
   const textures = useTexture([
-    "/splat.jpg",
+    "/splat-rgb.jpg",
     "/Grass_021/ground_Grass1_col.jpg",
     "/Grass_021/ground_Grass1_norm.jpg",
     "/Mud_030/Ground_WetBumpyMud_col.jpg",
     "/Mud_030/Ground_WetBumpyMud_norm.jpg",
+    "/Cliffs_02/Rock_DarkCrackyCliffs_col.png",
+    "/Cliffs_02/Rock_DarkCrackyCliffs_norm.png",
   ]);
 
-  return (
+  textures.map(repeatTexture);
+
+  const grass = {
+    diffuse: textures[1],
+    normal: textures[2],
+    repeat: 10,
+  };
+
+  const mud = {
+    diffuse: textures[3],
+    normal: textures[4],
+    repeat: 2,
+  };
+
+  const clif = {
+    diffuse: textures[5],
+    normal: textures[6],
+    repeat: 2,
+  };
+  console.log(textures[0].isTexture);
+
+  // example bw
+  return textures ? (
     <mesh position={[0, 0, 0]}>
       <planeBufferGeometry args={[4, 4, 10, 10]} />
-      <TerrainMaterial
-        map={textures[0]}
-        splats={[textures[0]]}
-        materials={[
-          // grass
-          {
-            diffuse: textures[1],
-            normal: textures[2],
-          },
-          // mud
-          {
-            diffuse: textures[3],
-            normal: textures[4],
-          },
-        ]}
-      />
+      <TerrainMaterial splatMode="rgb" map={textures[0]} splats={[textures[0]]} materials={[grass, mud, clif]} />
     </mesh>
-  );
+  ) : null;
 }
 
 function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ near: 0.001 }}>
       <OrbitControls />
       <ambientLight intensity={0.5} />
       <Terrain />
--- package/src/components/TerrainMaterial.tsx
+++ package/src/components/TerrainMaterial.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 import CustomShaderMaterial from "three-custom-shader-material";
-import { MeshStandardMaterial, Texture } from "three";
+import { Material, MeshStandardMaterial, RepeatWrapping, Texture } from "three";
 import glsl from "glslify";
 
 export type splatMaterial = {
-  diffuse: Texture;
+  diffuse?: Texture;
   normal?: Texture;
+  repeat?: Number;
 };
 
 export default function TerrainMaterial(props: {
@@ -13,9 +14,17 @@ export default function TerrainMaterial(props: {
   materials: splatMaterial[];
   map?: Texture;
   splats: Texture[];
+  splatMode?: "bw" | "rgb" | "rgba";
 }) {
   const diffuse = props.materials.map((v) => v.diffuse);
   const normal = props.materials.map((v) => v.normal);
+  const repeat = props.materials.map((v) => v.repeat || 1);
+  // const textures = [...props.splats, ...diffuse, ...normal].filter((v) => v);
+  // useMemo(() => textures.map(repeatTexture), [diffuse.length]);
+
+  if (props.materials.length > 2 && props.splatMode === "bw")
+    throw Error("use rgb or rgba textures if you have more than 2 materials");
+
   return (
     <CustomShaderMaterial
       baseMaterial={MeshStandardMaterial}
@@ -25,6 +34,7 @@ export default function TerrainMaterial(props: {
         uSplats: { value: props.splats },
         uDiffuse: { value: diffuse },
         uNormal: { value: normal },
+        uRepeat: { value: repeat },
       }}
       fragmentShader={glsl`
         precision mediump float;
@@ -36,14 +46,53 @@ export default function TerrainMaterial(props: {
         uniform sampler2D uSplats[${props.splats.length}];
         uniform sampler2D uDiffuse[${diffuse.length}];
         uniform sampler2D uNormal[${normal.length}];
+        uniform float uRepeat[${repeat.length}];
 
         void main(){
-          vec4 d1 = texture2D(uDiffuse[0], vUv);
-          vec4 d2 = texture2D(uDiffuse[1], vUv);
-          float alpha =  texture2D(uSplats[0], vUv).r;
-          csm_DiffuseColor = mix(d1, d2, alpha);
+          csm_DiffuseColor = normalize(${computeDiffuse(props.materials, props.splatMode)});
         }
       `}
     />
   );
+}
+
+const repeatTexture = (t) => (t.wrapS = t.wrapT = RepeatWrapping);
+
+const splatSize = {
+  bw: 2,
+  rgb: 3,
+  rgba: 4,
+};
+
+function computeDiffuse(materials, splatMode = "bw") {
+  if (splatMode === "bw") {
+    const r1 = materials[0].repeat || 1;
+    const r2 = materials[1].repeat || 1;
+    return `mix(
+      texture2D(uDiffuse[0], vUv * vec2(${r1},${r1})),
+      texture2D(uDiffuse[1], vUv * vec2(${r2},${r2})),
+      texture2D(uSplats[0], vUv).r
+    )`;
+  }
+
+  let d = 0;
+  return materials
+    .map((material, m) => {
+      if (material.diffuse) {
+        const r = material.repeat || 1;
+        const colorValue = `texture2D(uDiffuse[${d}], vUv * vec2(${r}, ${r}))`;
+        let weight = `texture2D(uSplats[${splatIndex(m, splatMode)}], vUv).${splatChannel(m, splatMode)}`;
+        d++;
+        return `${colorValue} * ${weight}`;
+      }
+    })
+    .filter((v) => v)
+    .join(" + ");
+}
+
+function splatIndex(i, splatMode) {
+  return Math.floor(i / splatSize[splatMode]);
+}
+function splatChannel(i, splatMode) {
+  return ["r", "g", "b", "a"][i % splatSize[splatMode]];
 }
